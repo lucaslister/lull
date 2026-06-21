46a4af9e7e64af55a848cf491af8b67bb85d51dc
lucaslister <lucaslister.09@gmail.com>
2026-06-22 09:22:37 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lull-v1';
+const CACHE = 'lull-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './glow.mp4', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
